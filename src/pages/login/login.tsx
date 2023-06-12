468ac73392f44e10d2fd914979090f4ea710bcdf
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-06-12 16:06:25 +0200

    add "keep me logged in" checkbox and logic behind it

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,21 +1,24 @@
 import LoginIcon from "@mui/icons-material/Login";
-import { Box, Button, Snackbar, TextField } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Checkbox, Snackbar, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
   const [error, setError] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    // Perform the user and password check here
     if (email === "chefs" && password === "chef-ivx1!") {
-      // Navigate to the /edit page
       navigate("/edit");
+
+      if (rememberMe) {
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("email", email);
+      }
     } else {
-      // Invalid user or password, display an error message
       setError(true);
     }
   };
@@ -23,6 +26,12 @@ function Login() {
   const handleCloseError = () => {
     setError(false);
   };
+
+  useEffect(() => {
+    if (localStorage.getItem("loggedIn") === "true") {
+      navigate("/edit");
+    }
+  }, []);
 
   return (
     <Box>
@@ -51,15 +60,23 @@ function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <Button
-        sx={{ width: 215, marginTop: 2, borderRadius: 5 }}
-        startIcon={<LoginIcon />}
-        variant="contained"
-        onClick={handleLogin}
-      >
-        Login
-      </Button>
-
+      <div>
+        <Checkbox
+          checked={rememberMe}
+          onChange={(e) => setRememberMe(e.target.checked)}
+        />
+        <span>Keep me logged in</span>
+      </div>
+      <div>
+        <Button
+          sx={{ width: 215, marginTop: 2, borderRadius: 5 }}
+          startIcon={<LoginIcon />}
+          variant="contained"
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+      </div>
       <Snackbar
         open={error}
         autoHideDuration={3000}
