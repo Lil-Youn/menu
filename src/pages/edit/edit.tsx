73ee77c850144de92b70fd2ee30af3cf2f1a6869
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-19 11:09:35 +0200

    rewrite code to update entrys via xata

--- src/pages/edit/edit.tsx
+++ src/pages/edit/edit.tsx
@@ -1,286 +1,93 @@
-import { Box, Button, Fab, TextField, Typography } from "@mui/material";
 import IosShareIcon from "@mui/icons-material/IosShare";
-import db from "../db.json";
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
+import { Box, Button, Fab, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { getXataClient } from "../../xata";
 
-interface FormData {
-  [key: string]: {
-    starter: string;
-    main: string;
-    sweet: string;
-  };
+const xata = getXataClient();
+
+const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+
+async function fetchMenu() {
+  const menu = await xata.db.menu.getMany();
+  return menu;
 }
 
 function Edit() {
-  const [updatedDb, setUpdatedDb] = useState(db);
-  const [formData, setFormData] = useState<FormData>({
-    Monday: { starter: "", main: "", sweet: "" },
-    Tuesday: { starter: "", main: "", sweet: "" },
-    Wednesday: { starter: "", main: "", sweet: "" },
-    Thursday: { starter: "", main: "", sweet: "" },
-    Friday: { starter: "", main: "", sweet: "" },
-  });
-  const [submissionStatus, setSubmissionStatus] = useState(false);
+  const [menu, setMenu] = useState(null);
 
-  const handleInputChange = (day: string, mealType: string, value: string) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [day]: {
-        ...prevState[day],
-        [mealType]: value,
-      },
-    }));
-  };
+  useEffect(() => {
+    fetchMenu().then((fetchedMenu) => setMenu(fetchedMenu));
+  }, []);
 
-  const handleSaveChanges = () => {
-    const updatedData: typeof db = { ...updatedDb };
+  async function updateField(day: string, field: string, newValue: string) {
+    const updatedRecord = await xata.db.menu.update(
+      "rec_cjdtq9tqdu05925101v0",
+      { [`${day}_${field}`]: newValue }
+    );
 
-    for (const day in formData) {
-      updatedData[day as keyof typeof db].starter = formData[day].starter;
-      updatedData[day as keyof typeof db].main = formData[day].main;
-      updatedData[day as keyof typeof db].sweet = formData[day].sweet;
-    }
-
-    localStorage.setItem("db", JSON.stringify(updatedData));
-    setUpdatedDb(updatedData);
-    setSubmissionStatus(true);
-
-    setTimeout(() => {
-      setSubmissionStatus(false);
-    }, 1000);
-  };
+    setMenu(updatedRecord);
+  }
 
   return (
     <>
-      <Box>
-        <Typography
-          sx={{ display: "flex", justifyItems: "flex-start" }}
-          variant="button"
-        >
-          Monday
-        </Typography>
-        <div
-          style={{
-            padding: "10px",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid gray",
-            marginBottom: "10px",
-          }}
-        >
-          <TextField
-            multiline
-            rows={2}
-            label="Starter"
-            value={formData.Monday.starter}
-            onChange={(e) =>
-              handleInputChange("Monday", "starter", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Main"
-            value={formData.Monday.main}
-            onChange={(e) =>
-              handleInputChange("Monday", "main", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Sweet"
-            value={formData.Monday.sweet}
-            onChange={(e) =>
-              handleInputChange("Monday", "sweet", e.target.value)
-            }
-          />
-        </div>
-
-        <Typography
-          sx={{ display: "flex", justifyItems: "flex-start" }}
-          variant="button"
-        >
-          Tuesday
-        </Typography>
-        <div
-          style={{
-            padding: "10px",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid gray",
-            marginBottom: "10px",
-          }}
-        >
-          <TextField
-            multiline
-            rows={2}
-            label="Starter"
-            value={formData.Tuesday.starter}
-            onChange={(e) =>
-              handleInputChange("Tuesday", "starter", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Main"
-            value={formData.Tuesday.main}
-            onChange={(e) =>
-              handleInputChange("Tuesday", "main", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Sweet"
-            value={formData.Tuesday.sweet}
-            onChange={(e) =>
-              handleInputChange("Tuesday", "sweet", e.target.value)
-            }
-          />
-        </div>
-
-        <Typography
-          sx={{ display: "flex", justifyItems: "flex-start" }}
-          variant="button"
-        >
-          Wednesday
-        </Typography>
-        <div
-          style={{
-            padding: "10px",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid gray",
-            marginBottom: "10px",
-          }}
-        >
-          <TextField
-            multiline
-            rows={2}
-            label="Starter"
-            value={formData.Wednesday.starter}
-            onChange={(e) =>
-              handleInputChange("Wednesday", "starter", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Main"
-            value={formData.Wednesday.main}
-            onChange={(e) =>
-              handleInputChange("Wednesday", "main", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Sweet"
-            value={formData.Wednesday.sweet}
-            onChange={(e) =>
-              handleInputChange("Wednesday", "sweet", e.target.value)
-            }
-          />
-        </div>
-
-        <Typography
-          sx={{ display: "flex", justifyItems: "flex-start" }}
-          variant="button"
-        >
-          Thursday
-        </Typography>
-        <div
-          style={{
-            padding: "10px",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid gray",
-            marginBottom: "10px",
-          }}
-        >
-          <TextField
-            multiline
-            rows={2}
-            label="Starter"
-            value={formData.Thursday.starter}
-            onChange={(e) =>
-              handleInputChange("Thursday", "starter", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Main"
-            value={formData.Thursday.main}
-            onChange={(e) =>
-              handleInputChange("Thursday", "main", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Sweet"
-            value={formData.Thursday.sweet}
-            onChange={(e) =>
-              handleInputChange("Thursday", "sweet", e.target.value)
-            }
-          />
-        </div>
-
-        <Typography
-          sx={{ display: "flex", justifyItems: "flex-start" }}
-          variant="button"
-        >
-          Friday
-        </Typography>
-        <div
-          style={{
-            padding: "10px",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid gray",
-            marginBottom: "10px",
-          }}
-        >
-          <TextField
-            multiline
-            rows={2}
-            label="Starter"
-            value={formData.Friday.starter}
-            onChange={(e) =>
-              handleInputChange("Friday", "starter", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Main"
-            value={formData.Friday.main}
-            onChange={(e) =>
-              handleInputChange("Friday", "main", e.target.value)
-            }
-          />
-          <TextField
-            multiline
-            rows={2}
-            label="Sweet"
-            value={formData.Friday.sweet}
-            onChange={(e) =>
-              handleInputChange("Friday", "sweet", e.target.value)
-            }
-          />
-        </div>
-      </Box>
+      {menu && (
+        <Box>
+          {daysOfWeek.map((day) => (
+            <div key={day}>
+              <Typography
+                sx={{ display: "flex", justifyItems: "flex-start" }}
+                variant="button"
+              >
+                {day.charAt(0).toUpperCase() + day.slice(1)}
+              </Typography>
+              <div
+                style={{
+                  padding: "10px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  border: "1px solid gray",
+                  marginBottom: "10px",
+                }}
+              >
+                <TextField
+                  multiline
+                  rows={2}
+                  label="Starter"
+                  value={menu[`${day}_starter`]}
+                  onChange={(event) =>
+                    updateField(day, "starter", event.target.value)
+                  }
+                />
+                <TextField
+                  multiline
+                  rows={2}
+                  label="Main"
+                  value={menu[`${day}_main`]}
+                  onChange={(event) =>
+                    updateField(day, "main", event.target.value)
+                  }
+                />
+                <TextField
+                  multiline
+                  rows={2}
+                  label="Sweet"
+                  value={menu[`${day}_sweet`]}
+                  onChange={(event) =>
+                    updateField(day, "sweet", event.target.value)
+                  }
+                />
+              </div>
+            </div>
+          ))}
+        </Box>
+      )}
 
       <Button
         sx={{ width: 215, marginTop: 0, borderRadius: 5 }}
         startIcon={<IosShareIcon />}
         variant="contained"
-        onClick={handleSaveChanges}
       >
         Submit
       </Button>
@@ -295,27 +102,6 @@ function Edit() {
           Show Menu
         </Fab>
       </NavLink>
-
-      {submissionStatus && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-            zIndex: 9999,
-          }}
-        >
-          <Typography variant="h4" style={{ color: "#fff" }}>
-            Data submitted successfully!
-          </Typography>
-        </div>
-      )}
     </>
   );
 }
