9812ddbac2e0bce57e3035bf83d25247d88b0955
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-19 11:08:52 +0200

    add query provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Edit from "./pages/edit/edit.tsx";
 import Login from "./pages/login/login.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const router = createBrowserRouter([
   {
@@ -22,9 +23,12 @@ const router = createBrowserRouter([
     element: <Login />,
   },
 ]);
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
